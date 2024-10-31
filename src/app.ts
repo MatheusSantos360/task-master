6d572bc776f5d809d0f76310a1425e22fd089e1c
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-10-31 21:32:11 +0000

    chore: express server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("It is working...");
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+})
+
+app.listen(3000, () => {
+  console.log("server is running on http://localhost:3000");
+});
