7f9f3fc7897d04034a9f074467e34f3dea49610d
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-11-25 22:50:08 +0000

    chore: improve userSchema

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,11 +1,36 @@
 import { z } from "zod";
 
-export const userSchema = z.object({
-  id: z.number(),
-  name: z.string().min(3, "Must be at least 3 characters long."),
-  email: z.string().email(),
-  password: z.string().min(8, "Must be at least 8 characters long."),
-  confirmPassword: z.string().min(8, "Must be at least 8 characters long."),
-});
+// Function to check password strength
+const isStrongPassword = (password: string) => {
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>_]/.test(password);
+  return password.length >= minLength && hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+};
 
-export type IUser = Omit<z.infer<typeof userSchema>, "confirmPassword">
+export const userSchema = z
+  .object({
+    id: z.number().nonnegative({ message: "Please enter a valid ID. It should be a non-negative number." }),
+    name: z
+      .string()
+      .min(3, { message: "Name must have at least 3 characters." })
+      .refine((val) => val !== undefined && val.trim() !== "", {
+        message: "Name can't be empty.",
+      }),
+    email: z.string().email({ message: "Please provide a valid email address." }),
+    password: z
+      .string()
+      .min(8, { message: "Password needs to be at least 8 characters long." })
+      .refine((val) => isStrongPassword(val), {
+        message: "Password must include uppercase and lowercase letters, a number, and a special character.",
+      }),
+    confirmPassword: z.string().min(8, { message: "Please confirm your password (at least 8 characters)." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match, please check again.",
+    path: ["confirmPassword"],
+  });
+
+export type IUser = Omit<z.infer<typeof userSchema>, "confirmPassword">;
