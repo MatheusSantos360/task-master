3a9fc2c62c39be631a042109e06c381c94d4b182
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-11-26 22:13:23 +0000

    chrore: add timestamps to the user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,12 +3,15 @@ import { IUser } from "../types/user.types";
 
 type UserDocument = IUser & Document;
 
-const UserSchema = new Schema<UserDocument>({
-  id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema<UserDocument>(
+  {
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const User = model<UserDocument>("users", UserSchema);
 export default User;
