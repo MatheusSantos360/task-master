439c59e3ce8352315119141ff02eea81a671384e
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-11-26 21:39:21 +0000

    chore: define creareUsers repository and controller types

---
+++ src/types/repositories/create-tasks/index.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from "../../http.types";
+
+export interface ICreateTasksController {
+  // @ts-expect-error ITask not implemented 
+  handle(body: ITask): Promise<HttpResponse<Itask>>;
+}
+
+export interface ICreateTasksRepository {
+  // @ts-expect-error ITask not implemented 
+  createTask(task: ITask): Promise<Itask[]>;
+}
