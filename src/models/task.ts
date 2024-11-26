7b551d83f660a53e2a7cc5ec180757c3cb86f001
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2024-11-26 22:41:53 +0000

    chore: create tasks model and improve tasks schema

---
+++ src/models/task.ts
@@ -0,0 +1,17 @@
+import { Document, model, Schema } from "mongoose";
+import { ITask } from "../types/task.types";
+
+type TaskDocument = ITask & Document;
+
+const tasksSchema = new Schema<TaskDocument>({
+  id: { type: Number, required: true, unique: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  dueDate: { type: Date },
+  priority: { type: String, enum: ["low", "medium", "high"], required: true },
+  status: { type: String, enum: ["pending", "in progress", "completed"], required: true },
+  user: { type: Number, required: true },
+});
+
+const Task = model<TaskDocument>("tasks", tasksSchema);
+export default Task;
--- src/types/task.types.ts
+++ src/types/task.types.ts
@@ -17,13 +17,39 @@ const statusValues = Object.values(Status) as [string, ...string[]];
 
 export const tasksSchema = z.object({
   id: z.number().nonnegative(),
-  title: z.string().min(1).max(100),
-  description: z.string().min(1).max(1000),
-  dueDate: z.date().optional(),
-  priority: z.enum(priorityValues).optional(),
-  status: z.enum(statusValues).optional(),
-  tags: z.array(z.string()).optional(),
+  title: z
+    .string()
+    .min(1, { message: "Title cannot be empty. Come on, give your task a cool name!" })
+    .max(100, { message: "Title cannot exceed 100 characters. Be concise and to the point!" }),
+  description: z
+    .string()
+    .min(1, { message: "Description cannot be empty. Give more details about the task!" })
+    .max(1000, { message: "Description cannot exceed 1000 characters. Keep it simple!" }),
+  dueDate: z
+    .date()
+    .optional()
+    .refine((date) => date === undefined || date > new Date(), {
+      message: "The due date must be a future date. Let's avoid delays!",
+    }),
+  priority: z
+    .enum(priorityValues)
+    .optional()
+    .refine((val) => val === undefined || priorityValues.includes(val), {
+      message: "Select a valid priority: High, Medium, or Low.",
+    }),
+  status: z
+    .enum(statusValues)
+    .optional()
+    .refine((val) => val === undefined || statusValues.includes(val), {
+      message: "Select a valid status: Todo, In Progress, or Completed.",
+    }),
+  tags: z
+    .array(z.string())
+    .optional()
+    .refine((tags) => tags === undefined || tags.every((tag) => typeof tag === "string"), {
+      message: "Tags must be text. Let's keep things organized!",
+    }),
+  user: z.number(),
 });
 
-
-export type ITask = z.infer<typeof tasksSchema>
+export type ITask = z.infer<typeof tasksSchema>;
